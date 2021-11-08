dce303f22d9ccedc04f687df38ebd9a742d42dac
Ricardo Pérez <ricpelo@gmail.com>
2021-11-08 19:33:11 +0100

    Genera PDF

--- pruebapaquetes/index.php
+++ pruebapaquetes/index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo "Hola\n";
+use Mpdf\Mpdf;
 
+require 'vendor/autoload.php';
+
+$mpdf = new Mpdf();
+$html = <<<EOT
+    <table border="1">
+        <thead>
+            <th>Columna</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Hola</td>
+            </tr>
+        </tbody>
+    </table>
+EOT;
+$mpdf->WriteHTML($html);
+$mpdf->Output();
